5c45f1b54bd8a3a3ca03aa8e33b72ff238633579
QusaiTA <tashtoshqusai@gmail.com>
2021-06-21 18:17:43 +0300

    fix remove item

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 'use strict';
+let ii=0;
 
 let allProduct = [];
 let tableHeader = ['ProductName', 'Category', 'Quantity', 'Price' , 'Remove'];
 let myForm = document.getElementById('myForm');
 let parent = document.getElementById('table');
 let total = document.getElementById('total');
+
 
 
 function FormProduct(name, category, quantity) {
@@ -35,22 +37,6 @@ function renderFirstRow() {
 
 }
 
-function handelSubmit(event) {
-    event.preventDefault();
-    let itemName = event.target.ProductName.value;
-    let itemCategory = event.target.productCategory.value;
-    let itemQuantity = event.target.productQuantity.value;
-
-    let newItem = new FormProduct(itemName, itemCategory, itemQuantity);
-    
-
-    localStorage.setItem('Products', JSON.stringify(allProduct));
-    newItem.renderItem();
-
-
-
-
-}
 
 FormProduct.prototype.renderItem = function () {
 
@@ -68,12 +54,23 @@ FormProduct.prototype.renderItem = function () {
     let itemPrice = document.createElement('td');
     itemPrice.textContent = this.price;
 
+    let deleteTd = document.createElement('td');
+    deleteTd.id=ii++;
+    
+
+        
+    deleteTd.textContent = 'X';
+        
+
+ 
+
     
 
     itemsRow.appendChild(itemName);
     itemsRow.appendChild(itemCategory);
     itemsRow.appendChild(itemQuantity);
     itemsRow.appendChild(itemPrice);
+    itemsRow.appendChild(deleteTd);
     parent.appendChild(itemsRow);
 
 
@@ -82,6 +79,26 @@ FormProduct.prototype.renderItem = function () {
 
 
 }
+myForm.addEventListener('submit', handelSubmit);
+
+function handelSubmit(event) {
+    event.preventDefault();
+    let itemName = event.target.ProductName.value;
+    let itemCategory = event.target.productCategory.value;
+    let itemQuantity = event.target.productQuantity.value;
+
+    let newItem = new FormProduct(itemName, itemCategory, itemQuantity);
+    
+
+    localStorage.setItem('Products', JSON.stringify(allProduct));
+    newItem.renderItem();
+    // myForm.reset();
+
+
+
+
+}
+
 
 
 
@@ -117,6 +134,13 @@ function renderAgain() {
         let itemPrice = document.createElement('td');
         itemPrice.textContent = allProduct[i].price;
 
+        let deleteTd = document.createElement('td');
+        deleteTd.id = i;
+        
+        deleteTd.textContent = 'X';
+        
+        
+
        
        
 
@@ -125,6 +149,7 @@ function renderAgain() {
         itemsRow.appendChild(itemCategory);
         itemsRow.appendChild(itemQuantity);
         itemsRow.appendChild(itemPrice);
+        itemsRow.appendChild(deleteTd);
         parent.appendChild(itemsRow);
 
 
@@ -154,8 +179,45 @@ function calculateTotal(){
 }
 
 
+// function removeItem (id){
+//     // Swal.fire('Item Removed from the Cart!')
+  
+//     allProduct.splice(id,1);
+    
+//     // updateCounter();
+  
+   
+//   }
 
-myForm.addEventListener('submit', handelSubmit);
+  parent.addEventListener('click', removeItemFromCart);
+
+  function removeItemFromCart(event) {
+    parent.textContent = '';  
+    alert('hi');
+    console.log(event.target.id);
+    if (event.target.textContent = 'X' ) {
+    //   removeItem(event.target.id);
+    allProduct.splice(event.target.id,1);
+
+     
+      console.log(event.target.textContent); 
+
+
+      }
+
+
+      localStorage.setItem('Products',JSON.stringify(allProduct));
+      allProduct = JSON.parse(localStorage.getItem('Products'));
+       
+      renderFirstRow();
+      renderAgain();
+        
+    }
+
+
+
+
+
 
 
 
